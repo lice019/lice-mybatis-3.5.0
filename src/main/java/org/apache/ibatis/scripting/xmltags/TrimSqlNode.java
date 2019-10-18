2923b1ac656e1689ed9c5f3655dd40316b5fea23
lice <11606537852@qq.com>
2019-10-18 19:14:02 +0800

    mybatis深入学习动态SQL

--- src/main/java/org/apache/ibatis/scripting/xmltags/TrimSqlNode.java
+++ src/main/java/org/apache/ibatis/scripting/xmltags/TrimSqlNode.java
@@ -1,18 +1,4 @@
-/**
- *    Copyright ${license.git.copyrightYears} the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
+
 package org.apache.ibatis.scripting.xmltags;
 
 import java.util.ArrayList;
@@ -25,6 +11,7 @@ import java.util.StringTokenizer;
 import org.apache.ibatis.session.Configuration;
 
 /**
+ * TrimSqlNode：是Mapper.xml中<trim></trim>标签的节点
  * @author Clinton Begin
  */
 public class TrimSqlNode implements SqlNode {
